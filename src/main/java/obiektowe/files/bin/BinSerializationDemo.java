64fcae09852a3263107c809fcddcf93ac788ed1f
Daniel Szlicht <daniel.szlicht25@gmail.com>
2022-10-13 18:59:58 +0200

    bin

---
+++ src/main/java/obiektowe/files/bin/BinSerializationDemo.java
@@ -0,0 +1,22 @@
+package obiektowe.files.bin;
+
+
+import java.io.*;
+
+public class BinSerializationDemo {
+    public static void main(String[] args) {
+        Engine engine = new Engine("TDI");
+        Car car = new Car("Volkswagen", "Passat",
+                180, 1.9, engine);
+        try {
+            ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("src/main/java/obiektowe/files/bin/car.bin"));
+            oos.writeObject(car);
+            oos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Nie udało się utworzyć pliku");
+        }
+
+
+    }
+}
--- src/main/java/obiektowe/files/text/WriteObjectDemo.java
+++ src/main/java/obiektowe/files/text/WriteObjectDemo.java
@@ -1,15 +1,14 @@
-package obiektowe.files;
+package obiektowe.files.text;
 
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
-import java.util.Objects;
 
 public class WriteObjectDemo {
     public static void main(String[] args) {
         Car car = new Car("Volkswagen", "Passat", 180, 1.9);
         System.out.println(car);
         try {
-            PrintWriter out = new PrintWriter("src/main/java/obiektowe/files/car2.csv"); // comma separeted values
+            PrintWriter out = new PrintWriter("src/main/java/obiektowe/files/text/car2.csv"); // comma separeted values
             out.println(car.toCsv());
             out.close();
         } catch (FileNotFoundException e) {
